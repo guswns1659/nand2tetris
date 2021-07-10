57a164d586a4577e570b8a6a9d2bcd3c743bbf16
guswns1659 <guswns1659@gmail.com>
2021-07-10 17:00:36 +0900

    Impl Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,46 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Multiply r = a*b
+
+// Start R2 at 0.
+@R2
+M=0
+
+// Jump into the first STEP if R0 > 0.
+@R0
+D=M
+@STEP
+D;JGT
+
+// If it didn't jump, go to END.
+@END
+0;JMP
+
+// Adds R1 to R2 and removes 1 from R0.
+// If R0 is more than 0 we step again.
+(STEP)
+    // Get R2.
+    @R2
+    D=M
+
+    // Add R1 to it.
+    @R1
+    D=D+M
+
+    // And write the result back to R2.
+    @R2
+    M=D
+
+    // Reduce R0 by 1.
+    @R0
+    D=M-1
+    M=D
+
+    // If R0 is still > 0 then loop.
+    @STEP
+    D;JGT
+
+(END)
+    @END
+    0;JMP
